4ac1f4b75a8ce3994e46483a4407ff6de347a68f
Nik Clayton <nik.clayton@powercoders.org>
2018-10-16 19:40:51 +0200

    Add Emacs configuration for editing the presentations.

---
+++ courseware/emacs.el
@@ -0,0 +1,53 @@
+;; Emacs config file for the slides
+
+;; TODO: Load Org
+
+(setq org-support-shift-select t)
+
+(setq css-indent-offset 2)
+
+;; Graphviz support
+(use-package graphviz-dot-mode
+  :ensure t)
+
+;; org-babel: Graphviz diagrams.
+(add-to-list 'org-src-lang-modes (quote ("dot" . graphviz-dot)))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((dot . t)))
+
+;; org-babel: Redisplay inline images after evaluation.
+(add-hook 'org-babel-after-execute-hook
+	  'org-redisplay-inline-images)
+
+;; org-babel: No need to confirm execution of dot
+(defun my/org-confirm-babel-evaluate (lang body)
+  (not (string= lang "dot")))
+(setq org-confirm-babel-evaluate 'my/org-confirm-babel-evaluate)
+
+;; Easy snippets to make code insertion easier
+
+(add-to-list 'org-structure-template-alist
+	     '("h" "#+BEGIN_SRC html
+?
+#+END_SRC"))
+(add-to-list 'org-structure-template-alist
+	     '("j" "#+BEGIN_SRC javascript
+?
+#+END_SRC"))
+(add-to-list 'org-structure-template-alist
+	     '("d" "#+BEGIN_SRC dot :file ?.svg :cmdline -Tsvg -Gstylesheet=../graphviz.css
+digraph G {
+
+}
+#+END_SRC"))
+
+(add-to-list 'org-structure-template-alist
+	     '("left" "#+REVEAL_HTML: <div class=\"leftcol\">
+?
+#+REVEAL_HTML: </div>"))
+(add-to-list 'org-structure-template-alist
+	     '("right" "#+REVEAL_HTML: <div class=\"rightcol\">
+?
+#+REVEAL_HTML: </div>"))
