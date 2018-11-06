4261771ff083426cb28e313fd37a893721c00345
Nik Clayton <nik.clayton@powercoders.org>
2018-11-06 23:34:39 +0100

    Expand the shopping list tutorial.
    - Refactoring
    - Scope
    - More on 'if'
    - String.prototype.trim()
    - Enabling / disabling buttons

---
+++ courseware/week2/shopping/shopping-4.js
@@ -0,0 +1,44 @@
+/**
+ * Creates and returns an 'li' element for inclusion in the shopping list.
+ *
+ * @param {string} itemName Name of the item to add to the list
+ * @returns {HTMLElement} li element
+ */
+function createNewListItem(itemName) {
+  let listItem = document.createElement('li');
+
+  let listText = document.createElement('span');
+  listText.textContent = itemName;
+
+  let deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  deleteButton.addEventListener('click', function (event) {
+    listItem.remove();
+  });
+
+  listItem.appendChild(listText);
+  listItem.appendChild(deleteButton);
+
+  return listItem;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  let inputBox = document.getElementById('item');
+  let shoppingList = document.querySelector('ul');
+
+  document.querySelector('button').addEventListener('click', function (event) {
+    shoppingList.appendChild(createNewListItem(inputBox.value));
+    inputBox.value = '';
+    inputBox.focus();
+  });
+
+  document.querySelector('input').addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+      shoppingList.appendChild(createNewListItem(inputBox.value));
+      inputBox.value = '';
+    }
+  });
+
+  inputBox.focus();
+});
---
+++ courseware/week2/shopping/shopping-7.js
@@ -0,0 +1,55 @@
+/**
+ * Creates and returns an 'li' element for inclusion in the shopping list.
+ *
+ * @param {string} itemName Name of the item to add to the list
+ * @returns {HTMLElement} li element
+ */
+function createNewListItem(itemName) {
+  let listItem = document.createElement('li');
+
+  let listText = document.createElement('span');
+  listText.textContent = itemName;
+
+  let deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  deleteButton.addEventListener('click', function(event) {
+    listItem.remove();
+  });
+
+  listItem.appendChild(listText);
+  listItem.appendChild(deleteButton);
+
+  return listItem;
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  let inputBox = document.getElementById('item');
+  let shoppingList = document.querySelector('ul');
+  let addItemButton = document.querySelector('button');
+
+  document.querySelector('button').addEventListener('click', function(event) {
+    if (inputBox.value.trim() !== '') {
+      shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
+      inputBox.value = '';
+      addItemButton.disabled = true;
+    }
+    inputBox.focus();
+  });
+
+  document.querySelector('input').addEventListener('keyup', function(event) {
+    if (inputBox.value.trim() !== '') {
+      addItemButton.disabled = false;
+      if (event.key === 'Enter') {
+        shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
+        inputBox.value = '';
+      }
+    }
+
+    if (inputBox.value.trim() === '') {
+      addItemButton.disabled = true;
+    }
+  });
+
+  inputBox.focus();
+});
