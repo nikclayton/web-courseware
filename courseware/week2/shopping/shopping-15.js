4710b8ccd767cb5b51198c6a4599b9e2a6aff5e2
Nik Clayton <nik.clayton@powercoders.org>
2019-01-03 19:07:54 +0100

    Arrays, NodeLists, and forEach().

---
+++ courseware/week2/shopping/shopping-15.js
@@ -0,0 +1,79 @@
+/**
+ * Creates and returns an 'li' element for inclusion in the shopping list.
+ *
+ * @param {string} itemName Name of the item to add to the list
+ * @returns {HTMLElement} li element
+ */
+function createNewListItem(itemName) {
+  const listItem = document.createElement('li');
+
+  const listText = document.createElement('span');
+  listText.textContent = itemName;
+
+  const deleteButton = document.createElement('button');
+  const trashIcon = document.createElement('i');
+  trashIcon.className = 'fas fa-trash';
+  deleteButton.appendChild(trashIcon);
+  //deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+
+  deleteButton.addEventListener('click', function (event) {
+    listItem.remove();
+
+    document.querySelector('button#clear').disabled =
+        document.querySelectorAll('li').length === 0;
+  });
+
+  listItem.appendChild(listText);
+  listItem.appendChild(deleteButton);
+
+  return listItem;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  const inputBox = document.getElementById('item');
+  const shoppingList = document.querySelector('ul');
+  const addItemButton = document.querySelector('button#add');
+  const clearListButton = document.querySelector('button#clear');
+
+  addItemButton.addEventListener('click', function (event) {
+    const trimmedValue = inputBox.value.trim();
+
+    if (trimmedValue === '') {
+      return;
+    }
+
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    inputBox.value = '';
+    addItemButton.disabled = true;
+    clearListButton.disabled = false;
+    inputBox.focus();
+  });
+
+  inputBox.addEventListener('keyup', function (event) {
+    const trimmedValue = inputBox.value.trim();
+    addItemButton.disabled = trimmedValue === '';
+
+    if (trimmedValue === '') {
+      return;
+    }
+
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    clearListButton.disabled = false;
+    inputBox.value = '';
+  });
+
+  clearListButton.addEventListener('click', function (event) {
+    const listItems = document.querySelectorAll('li');
+    listItems.forEach(function (element) {
+      element.remove();
+    });
+    clearListButton.disabled = true;
+  });
+
+  inputBox.focus();
+  clearListButton.disabled = true;
+});
