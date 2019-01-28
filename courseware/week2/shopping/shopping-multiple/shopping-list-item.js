e63b64eeadb698de6085de3a9ee323e1c5013a6a
Nik Clayton <nik.clayton@powercoders.org>
2019-01-28 07:57:47 +0100

    Show how to split JS in to multiple files.

---
+++ courseware/week2/shopping/shopping-multiple/shopping-list-item.js
@@ -0,0 +1,57 @@
+/**
+ * Represents an item in the shopping list.
+ */
+class ShoppingListItem {
+  /**
+   * @param {string} name Name of the item
+   * @param {string} quantity Quantity of the item
+   */
+  constructor(name, quantity) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+
+  /**
+   * Creates and returns an 'li' element for inclusion in the shopping list.
+   * @returns {HTMLElement} li element
+   */
+  toListItem() {
+    const listItem = document.createElement('li');
+
+    const listText = document.createElement('span');
+    listText.textContent = this.name;
+
+    const deleteButton = document.createElement('button');
+    const trashIcon = document.createElement('i');
+    trashIcon.className = 'fas fa-trash';
+    deleteButton.appendChild(trashIcon);
+
+    deleteButton.addEventListener('click', function (event) {
+      listItem.remove();
+
+      document.querySelector('button#clear').disabled =
+          document.querySelectorAll('li').length === 0;
+    });
+
+    listItem.appendChild(listText);
+
+    if (this.quantity) {
+      listItem.appendChild(document.createTextNode(' '));
+      const quantityText = document.createElement('span');
+      quantityText.textContent = `(${this.quantity})`;
+      listItem.appendChild(quantityText);
+    }
+
+    listItem.appendChild(deleteButton);
+
+    return listItem;
+  }
+
+  /**
+   * String representation of a ShoppingListItem.
+   * @returns {string}
+   */
+  toString() {
+    return `${this.name} ({$this.quantity})`;
+  }
+}
