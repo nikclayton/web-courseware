87fb6c00db39c0a3b849c5ac2c8b1e6d2d950474
Nik Clayton <nik.clayton@powercoders.org>
2019-04-17 23:02:34 +0200

    Clean up DOMContentLoaded.
    The DOMContentLoaded event was confusing to the class, and unnecessary
    if you keep the script element just inside the body.

--- courseware/week2/change_para_class/refactor-4-2.js
+++ courseware/week2/change_para_class/refactor-4-2.js
@@ -2,16 +2,14 @@ function setClass(className) {
   document.getElementById('mypara').className = className;
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('make-warning').addEventListener('click', function(event) {
-    setClass('warning');
-  });
+document.getElementById('make-warning').addEventListener('click', function(event) {
+  setClass('warning');
+});
 
-  document.getElementById('make-tip').addEventListener('click', function(event) {
-    setClass('tip');
-  });
+document.getElementById('make-tip').addEventListener('click', function(event) {
+  setClass('tip');
+});
 
-  document.getElementById('make-normal').addEventListener('click', function(event) {
-    setClass('');
-  });
+document.getElementById('make-normal').addEventListener('click', function(event) {
+  setClass('');
 });
