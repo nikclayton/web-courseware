25d7ed950dcb383dcdd9709e5a9a4898ae526f61
Nik Clayton <nik.clayton@powercoders.org>
2019-07-31 17:06:26 +0200

    Add less.

--- courseware/week2/shopping/shopping-node/Gruntfile.js
+++ courseware/week2/shopping/shopping-node/Gruntfile.js
@@ -8,65 +8,75 @@ module.exports = function (grunt) {
         mangle: true
       },
       build: {
-        src: 'src/js/*.js',
+//        src: ['src/js/view.js', 'src/js/model.js', 'src/js/controller.js',
+//          'src/js/shopping-list-item.js', 'src/js/ui.js'],
+        src: ['src/js/*.js', '!src/js/ui.js', 'src/js/ui.js'],
         dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    cssmin: {
-      target: {
-        // Minimise everything in to one
+      },
+      cssmin: {
+        target: {
+          // Minimise everything in to one
 //        files: {
 //          'build/css/style.min.css': ['src/css/*.css']
 //        }
-        // Minimise each file separately
-        files: [{
-          expand: true,
-          cwd: 'src/css',
-          src: ['*.css'],
-          dest: 'build/css',
-          ext: '.min.css'
-        }]
-      }
-    },
-    copy: {
-      html: {
-        src: 'src/html/index.prod.html',
-        dest: 'build/index.html'
+          // Minimise each file separately
+          files: [{
+            expand: true,
+            cwd: 'src/css',
+            src: ['*.css'],
+            dest: 'build/css',
+            ext: '.min.css'
+          }]
+        }
       },
-      static: {
-        files: [{
-          expand: true,
-          cwd: 'src/static',
-          src: '*.*',
-          dest: 'build/static',
-        }]
-      }
-    },
-    watch: {
-      css: {
-        files: 'src/css/*.css',
-        tasks: ['cssmin']
+      copy: {
+        html: {
+          src: 'src/html/index.prod.html',
+          dest: 'build/index.html'
+        },
+        static: {
+          files: [{
+            expand: true,
+            cwd: 'src/static',
+            src: '*.*',
+            dest: 'build/static',
+          }]
+        }
       },
-      scripts: {
-        files: 'src/js/*.js',
-        tasks: ['uglify']
+      less: {
+        build: {
+          files: {
+            'src/css/headings.css': 'src/css/headings.less'
+          }
+        }
       },
-      html: {
-        files: 'src/html/index.prod.html',
-        tasks: ['copy:html']
-      },
-      static: {
-        files: 'src/static/*.*',
-        tasks: ['copy:static']
+      watch: {
+        css: {
+          files: 'src/css/*.css',
+          tasks: ['cssmin']
+        },
+        scripts: {
+          files: 'src/js/*.js',
+          tasks: ['uglify']
+        },
+        html: {
+          files: 'src/html/index.prod.html',
+          tasks: ['copy:html']
+        },
+        static: {
+          files: 'src/static/*.*',
+          tasks: ['copy:static']
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['cssmin', 'uglify', 'copy']);
+  grunt.registerTask('default', ['less', 'cssmin', 'uglify', 'copy']);
 };
